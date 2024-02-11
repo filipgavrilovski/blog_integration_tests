9b9ee7b4594b3fe17936ac63c1ba9df543f86b5a
filipgavrilovski <filipgavrilovski12@gmail.com>
2024-02-11 19:45:58 +0100

    Fixed failing spec

--- cypress/e2e/createArticles.cy.js
+++ cypress/e2e/createArticles.cy.js
@@ -18,7 +18,6 @@ describe('New Article Page tests', () => {
     }
     ArticlesPage.newArticleButton().click()
     cy.log("CREATE THE NEW ARTICLE").then(() => {
-      ArticleDetailsPage.allArticlesButton().click()
       NewArticlePage.articleTitleInputField().type(newArticleData.articleTitle)
       NewArticlePage.articleBodyTextField().type(newArticleData.articleBody)
       NewArticlePage.statusSelectField().select(newArticleData.articleStatus)
